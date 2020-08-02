e7f8f28aa439ed548fc4c8f67c9c868f82f1694b
Roberto <roberto.araujo@logusinfo.com.br>
2020-08-01 21:29:34 -0300

    Added Edit and Update routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var Campground = require("../models/campgrounds");
+const { route } = require("./comments");
 
 // INDEX - Show all Campgrounds
 router.get("/", function(req, res) {
@@ -52,6 +53,28 @@ router.get("/:id", function(req, res) {
         }
     })
 });
+
+// EDIT CAMPGROUND ROUTE
+router.get("/:id/edit", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            res.redirect("/campgrounds");
+        } else {
+            res.render("campgrounds/edit", {campground: foundCampground});
+        }
+    })
+});
+
+// UPDATE CAMPGROUND ROUTE
+router.put("/:id", function(req, res){
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+        if(err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    })
+})
 
 // Middleware
 function isLoggedIn(req, res, next){
