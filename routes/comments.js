8ea049f5e1f2d4ebf6bf406eb84ffdbf9937155d
Roberto <roberto.araujo@logusinfo.com.br>
2020-08-09 21:45:45 -0300

    Added flash messages to routes

--- routes/comments.js
+++ routes/comments.js
@@ -26,16 +26,18 @@ router.post("/", middleware.isLoggedIn, function(req, res){
         } else {
             var newComment = Comment.create(req.body.comment, function(err, comment){
                 if(err) {
+                    req.flash("error", "Something went wrong");
                     console.log(err);
                 } else { 
                     //add username and id to comment
-                    //save comment
                     comment.author.id = req.user._id;
                     comment.author.username = req.user.username;
+                    //save comment
                     comment.save();
                     console.log(comment);
                     campground.comments.push(comment);
                     campground.save();
+                    req.flash("success", "Successfully added comment!");
                     res.redirect("/campgrounds/" + campground._id);
                 }
             });
@@ -43,6 +45,7 @@ router.post("/", middleware.isLoggedIn, function(req, res){
     });
 });
 
+// COMMENT EDIT ROUTE
 router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, res){
     Comment.findById(req.params.comment_id, function(err, foundComment){
         if(err){
@@ -70,6 +73,7 @@ router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, re
         if(err){
             res.redirect("back");
         } else {
+            req.flash("sucess", "Comment deleted");
             res.redirect("/campgrounds/" + req.params.id);
         }
     });
--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get("/", function(req, res){
 
 // ===================
 // AUTH ROUTES
-// ===================
+// =================== 
 
 // Show Register Form
 router.get("/register", function(req, res){
@@ -22,10 +22,11 @@ router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
         if(err){
-            console.log(err);
+            req.flash("error", err.message);
             return res.render("register");
         } 
         passport.authenticate("local")(req, res, function(){
+            req.flash("success", "Welcome to YelpCamp " + user.username);
             res.redirect("/campgrounds");
         });
     });
@@ -33,7 +34,7 @@ router.post("/register", function(req, res){
 
 // Login route
 router.get("/login", function(req, res){
-    res.render("login");
+    res.render("login", {message: req.flash("error")});
 });
 
 // Login Logic
@@ -47,15 +48,8 @@ router.post("/login", passport.authenticate("local",
 // Logout route
 router.get("/logout", function(req, res){
     req.logout();
+    req.flash("success", "Logged you out");
     res.redirect("/campgrounds");
 });
-
-// Middleware
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
 
 module.exports = router;
